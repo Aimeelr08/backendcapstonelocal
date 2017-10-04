5570c04a99de6369d3e9d33b441c706b4852620a
Aimeelr <aimeelynnramirez@gmail.com>
2017-10-04 18:50:15 -0400

    calling routed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+post_attributes = [
+  { title: "Sample Title 1", body: "Sample body 1" },
+  { title: "Sample Title 2", body: "Sample body 2" },
+  { title: "Sample Title 3", body: "Sample body 3" },
+  { title: "Another Post", body: "WOW!" },
+]
+
+post_attributes.each do |attributes|
+  Post.where(attributes).first_or_create
+end
