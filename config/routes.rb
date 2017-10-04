380822164b4fc46a2f64cd1eece85fedb5144b47
Aimeelr <aimeelynnramirez@gmail.com>
2017-10-04 11:15:45 -0400

    removing the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
   resources :comments
-  # root to: "posts#index"
-  resources :posts
+
+
   # root to: "posts#new"
   # root :to => "posts#index", only: [:get, :show]
   # root :to => "root#root"
 
 
-  get 'posts' => 'posts'
-  get 'comments' => 'comments'
-
+  get '/posts' => 'posts#index'
+  get '/comments' => 'comments#index'
   resources :examples, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
