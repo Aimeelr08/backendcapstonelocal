3ed9107ea0583bbbf3e8896ed2b678680afc991b
Aimeelr <aimeelynnramirez@gmail.com>
2017-10-05 16:08:12 -0400

    routes changed the call

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  # resources :comments
-  # resources :posts
+  resources :comments
+  resources :posts
   # please fix this TODO
   # root to: "posts#new"
   # root :to => "posts#index", only: [:get, :show]
