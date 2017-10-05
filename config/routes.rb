17dc8e25f8378185ef27ba503d5192b056931c45
Aimeelr <aimeelynnramirez@gmail.com>
2017-10-05 13:26:36 -0400

    covered the routes and ran rake test and zero failures

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,22 @@
 Rails.application.routes.draw do
   resources :comments
   resources :posts
-
-
   # root to: "posts#new"
   # root :to => "posts#index", only: [:get, :show]
   # root :to => "root#root"
-
-
+# making routes to posts
   get '/posts' => 'posts#index'
+  post '/posts' => 'posts#create'
+  post '/posts' => 'posts#update'
+  delete '/posts' => 'posts#destroy'
+  patch '/posts' => 'posts#update'
+  # making routes to comments
   get '/comments' => 'comments#index'
+  post '/comments' => 'comments#create'
+  post '/comments' => 'comments#update'
+  delete '/comments' => 'comments#destroy'
+  patch '/comments' => 'comments#update'
+  # # making routes to forms
   resources :examples, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
