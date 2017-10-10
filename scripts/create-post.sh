ca7ebe7e58223090c297b063fc1697f922845ad9
Aimeelr <aimeelynnramirez@gmail.com>
2017-10-10 01:29:26 -0400

    client origin

--- scripts/create-post.sh
+++ scripts/create-post.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-API="${API_ORIGIN:-http://localhost:4741/}"
+# API="${API_ORIGIN:-http://localhost:4741/}"
 
-# API="${API_ORIGIN:-https://backendcapstone.herokuapp.com}"
+API="${API_ORIGIN:-https://backendcapstone.herokuapp.com}"
 URL_PATH="/posts"
 curl "${API}${URL_PATH}" \
   --include \
--- scripts/users.sh
+++ scripts/users.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+API="${API_ORIGIN:-https://backendcapstone.herokuapp.com}"
 
-API="${API_ORIGIN:-http://localhost:4741}"
+# API="${API_ORIGIN:-http://localhost:4741}"
 URL_PATH="/users"
 curl "${API}${URL_PATH}" \
   --include \
